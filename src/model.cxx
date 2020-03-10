335824eae79e79140934246106f5f64139cc0748
lilyyan2023 <56418644+lilyyan2023@users.noreply.github.com>
2020-03-10 12:51:57 -0500

    meeting in main

--- src/model.cxx
+++ src/model.cxx
@@ -57,70 +57,70 @@ void Model::update() {
 //2. increment the score based on the type of the apple the snake eats
 //3. change the body color of the snake
 void Model::eat_apple() {
-    if (level_ == 1) {
-        //level 1 : 120 (subject to change)
-        // TODO: Can store the constants in geometry_ so it's easier to manage.
-        // TODO: Then you don't have to separate the cases.
-        while (score_ < 120) {
-            int round = 1;
-            int normal_apple = 0;
-            int timed_apple = 0;
-            score_ = snake_len() * 5 + normal_apple * 1 + timed_apple * 10;
-            if (snake_head() == apple_) {
-                // TODO: this is the wrong grid to push.
-                snake_.push_back(snake_tail() + dir_);
-                // TODO: didn't get this - I used a different method in proposal, but you can also explain yours.
-                if (round % 5 != 0) {
-                    normal_apple++;
-                } else {
-                    timed_apple++;
-                }
-                round++;
-            }
-            //change color per 20 points TODO: this is done in UI.
-        }
-
-    }
-    if (level_ == 2){
-        //level 2 : 300 (subject to change)
-        while (score_ < 300){
-            int round = 1;
-            int normal_apple = 0;
-            int timed_apple = 0;
-            score_ = snake_len() * 5 + normal_apple * 1 + timed_apple * 10;
-            if (snake_head() == apple_){
-                snake_.push_back(snake_tail() + dir_);
-                if (round % 5 != 0){
-                    normal_apple ++;
-                }
-                else{
-                    timed_apple ++;
-                }
-                round++;
-            }
-        }
-        //change color per 50 points
-    }
-    if (level_ == 3){
-        //level 3 : 600 (subject to change)
-        while (score_ < 600){
-            int round = 1;
-            int normal_apple = 0;
-            int timed_apple = 0;
-            score_ = snake_len() * 5 + normal_apple * 1 + timed_apple * 10;
-            if (snake_head() == apple_){
-                snake_.push_back(snake_tail() + dir_);
-                if (round % 5 != 0){
-                    normal_apple ++;
-                }
-                else{
-                    timed_apple ++;
-                }
-                round++;
-            }
-        }
-        //change color per 100 points
-    }
+//    if (level_ == 1) {
+//        //level 1 : 120 (subject to change)
+//        // TODO: Can store the constants in geometry_ so it's easier to manage.
+//        // TODO: Then you don't have to separate the cases.
+//        while (score_ < 120) {
+//            int round = 1;
+//            int normal_apple = 0;
+//            int timed_apple = 0;
+//            score_ = snake_len() * 5 + normal_apple * 1 + timed_apple * 10;
+//            if (snake_head() == apple_) {
+//                // TODO: this is the wrong grid to push.
+//                snake_.push_back(snake_tail() + dir_);
+//                // TODO: didn't get this - I used a different method in proposal, but you can also explain yours.
+//                if (round % 5 != 0) {
+//                    normal_apple++;
+//                } else {
+//                    timed_apple++;
+//                }
+//                round++;
+//            }
+//            //change color per 20 points TODO: this is done in UI.
+//        }
+//
+//    }
+//    if (level_ == 2){
+//        //level 2 : 300 (subject to change)
+//        while (score_ < 300){
+//            int round = 1;
+//            int normal_apple = 0;
+//            int timed_apple = 0;
+//            score_ = snake_len() * 5 + normal_apple * 1 + timed_apple * 10;
+//            if (snake_head() == apple_){
+//                snake_.push_back(snake_tail() + dir_);
+//                if (round % 5 != 0){
+//                    normal_apple ++;
+//                }
+//                else{
+//                    timed_apple ++;
+//                }
+//                round++;
+//            }
+//        }
+//        //change color per 50 points
+//    }
+//    if (level_ == 3){
+//        //level 3 : 600 (subject to change)
+//        while (score_ < 600){
+//            int round = 1;
+//            int normal_apple = 0;
+//            int timed_apple = 0;
+//            score_ = snake_len() * 5 + normal_apple * 1 + timed_apple * 10;
+//            if (snake_head() == apple_){
+//                snake_.push_back(snake_tail() + dir_);
+//                if (round % 5 != 0){
+//                    normal_apple ++;
+//                }
+//                else{
+//                    timed_apple ++;
+//                }
+//                round++;
+//            }
+//        }
+//        //change color per 100 points
+//    }
 }
 //
 
